7612c08ceef6594d29e4c2db82f65a72e486ee30
AntiCake <lightpresents@gmail.com>
2018-09-28 20:10:31 +0200

    Heroku again

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-var PORT = 1337;
+//var PORT = 1337;
 
 var express = require('express');
 var app = express();
 //var server = require('http').Server(app);
 
 var dgram = require('dgram');
-var WebSocket= require('ws');
-var wss = new WebSocket.Server({port: PORT});
+//var WebSocket= require('ws');
+//var wss = new WebSocket.Server({port: PORT});
 
 app.use(express.static("public"));
-/*app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});*/
 
-wss.on('connection', function(ws) {
+/*wss.on('connection', function(ws) {
 	ws.on('message', function (message) {
 		var msgBuff = Buffer.from(message);
 		console.log(message);
@@ -23,8 +20,8 @@ wss.on('connection', function(ws) {
 			}
 		});
 	});
-});
+});*/
 
-app.listen(3000, function(){
-  console.log('listening on *:' + (3000));
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:' + (process.env.PORT || 3000));
 });
