aec4af3fcf5381115286c46b6d2146269f22261d
AntiCake <lightpresents@gmail.com>
2018-09-28 21:39:08 +0200

    Experimenting with fullscreen and window resizing on PC and Android

--- public/js/game.js
+++ public/js/game.js
@@ -1,16 +1,51 @@
 var config = { 
     type: Phaser.AUTO, 
-    width: 344, 
-    height: 500, 
+    width: window.innerWidth * window.devicePixelRatio, 
+    height: window.innerHeight * window.devicePixelRatio, 
     scene: { 
         preload: preload, 
         create: create, 
         update: update
     } 
 }; 
+
 var game = new Phaser.Game(config); 
+var req;
+
 function preload () { } 
 function create () {
-this.add.rectangle(400, 300, 500, 120, 0x00ff00);
+	resize();
+	window.addEventListener('resize', resize, false);
+	window.addEventListener('click', gofull);
+	window.addEventListener('touchstart', gofull);
+	this.add.rectangle(400, 300, 500, 120, 0x00ff00);
+	text = this.add.text(16, 16, 'request: ', {fontSize: '32px', fill: '#fff'});
 } 
+
 function update () { }
+
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
+function gofull() {
+	var el = document.getElementsByTagName('canvas')[0];
+	var requestFullscreen = el.requestFullScreen || el.msRequestFullScreen || el.mozRequestFullScreen || el.webkitRequestFullScreen;
+	//this.canvas[this.device.fullscreen.request]();
+	if(requestFullscreen)
+		requestFullscreen.call(el);
+	text.setText('request: ' + requestFullscreen);
+}
+
